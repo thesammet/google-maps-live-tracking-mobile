efe286bffeb60418e1e449f7ae879ae00b78d363
thesammet <thesammet@gmail.com>
2024-02-05 19:23:56 +0300

    addition: custom map marker

--- App.js
+++ App.js
@@ -2,66 +2,71 @@ import React, { useState, useRef } from 'react'
 import { View, StyleSheet, Dimensions } from 'react-native'
 import MapView, { Marker } from 'react-native-maps';
 import MapViewDirections from 'react-native-maps-directions';
+import imagePath from './src/constants/imagePath';
 import { GOOGLE_MAP_KEY } from './src/constants/keys';
 
 const { width, height } = Dimensions.get('window');
 
 export default function App() {
-  const [state, setState] = useState(
-    {
-      pickupCoords: {
-        latitude: 39.9359739,
-        longitude: 32.8477795,
-        latitudeDelta: 0.0922,
-        longitudeDelta: 0.0421,
-      },
-      dropLocationCoords: {
-        latitude: 39.97100310,
-        longitude: 33.11711860,
-        latitudeDelta: 0.0922,
-        longitudeDelta: 0.0421,
-      }
-    }
-  )
+    const [state, setState] = useState(
+        {
+            pickupCoords: {
+                latitude: 39.9359739,
+                longitude: 32.8477795,
+                latitudeDelta: 0.0922,
+                longitudeDelta: 0.0421,
+            },
+            dropLocationCoords: {
+                latitude: 39.97100310,
+                longitude: 33.11711860,
+                latitudeDelta: 0.0922,
+                longitudeDelta: 0.0421,
+            }
+        }
+    )
 
-  const mapRef = useRef()
-  const { pickupCoords, dropLocationCoords } = state
+    const mapRef = useRef()
+    const { pickupCoords, dropLocationCoords } = state
 
-  return (
-    <View style={styles.container}>
-      <MapView
-        ref={mapRef}
-        style={StyleSheet.absoluteFill}
-        initialRegion={pickupCoords}
-      >
-        <Marker coordinate={pickupCoords} />
-        <Marker coordinate={dropLocationCoords} />
-        <MapViewDirections
-          origin={pickupCoords}
-          destination={dropLocationCoords}
-          apikey={GOOGLE_MAP_KEY}
-          strokeWidth={5}
-          strokeColor={"cyan"}
-          optimizeWaypoints={true}
-          onReady={result => {
-            mapRef.current.fitToCoordinates(result.coordinates, {
-              edgePadding: {
-                right: (width / 20),
-                bottom: (height / 20),
-                left: (width / 20),
-                top: (height / 20),
-              }
-            })
-          }}
-        />
-      </MapView>
-    </View>
+    return (
+        <View style={styles.container}>
+            <MapView
+                ref={mapRef}
+                style={StyleSheet.absoluteFill}
+                initialRegion={pickupCoords}
+            >
+                <Marker
+                    coordinate={pickupCoords}
+                    image={imagePath.icCurLoc} />
+                <Marker
+                    coordinate={dropLocationCoords}
+                    image={imagePath.icGreenMarker} />
+                <MapViewDirections
+                    origin={pickupCoords}
+                    destination={dropLocationCoords}
+                    apikey={GOOGLE_MAP_KEY}
+                    strokeWidth={5}
+                    strokeColor={"cyan"}
+                    optimizeWaypoints={true}
+                    onReady={result => {
+                        mapRef.current.fitToCoordinates(result.coordinates, {
+                            edgePadding: {
+                                right: (width / 20),
+                                bottom: (height / 20),
+                                left: (width / 20),
+                                top: (height / 20),
+                            }
+                        })
+                    }}
+                />
+            </MapView>
+        </View>
 
-  )
+    )
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  }
+    container: {
+        flex: 1,
+    }
 })
